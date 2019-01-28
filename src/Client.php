ee33904104b568190342724ba677e158bad77ab7
Ben Sherred <ben@sherred.co.uk>
2019-01-28 17:03:23 +0000

    We can now call a specified resource

--- src/Client.php
+++ src/Client.php
@@ -2,15 +2,31 @@
 
 namespace ToucanText;
 
+use mysql_xdevapi\Exception;
+
 class Client
 {
     // Authentication Params
     private $username;
     private $password;
 
-    public function __get($name)
+    /**
+     * __get overloads the client with a property that will check if there is a resource for the given $method
+     * which allows calls such as $toucan->messages->send() to be correctly routed to the appropriate handler
+     */
+    public function __get($method)
     {
-        // TODO: Implement __get() method.
+        $potentialEndpointClass = 'ToucanText\Resources\\' . ucfirst($method);
+
+        if (class_exists($potentialEndpointClass)) {
+            // construct a resource object and pass in this client
+            $resource = new $potentialEndpointClass($this);
+            return $resource;
+        }
+
+        $trace = debug_backtrace();
+        $message = 'Undefined property via __get(): ' . $method . ' in ' . $trace[0]['file'] . ' on line ' . $trace[0]['line'];
+        throw new Exceptions\InvalidResourceException($message);
     }
 
     /**
