a919426558b0ba0fc6fc894313f1d709f1eee984
Ben Sherred <ben@sherred.co.uk>
2019-01-29 10:06:52 +0000

    Added the ability to get a specific type of message

--- src/Resources/Messages.php
+++ src/Resources/Messages.php
@@ -2,6 +2,8 @@
 
 namespace ToucanText\Resources;
 
+use mysql_xdevapi\Exception;
+
 class Messages
 {
     private $client;
@@ -41,9 +43,39 @@ class Messages
         return $response->getBody();
     }
 
-    public function get()
+    /**
+     * Get the specified messages (inbound OR delivery receipts) only
+     *
+     * @param  string  $messageType
+     * @param  int  $maxMessageCount
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get($messageType = null, $maxMessageCount = 25)
     {
-        //
+        if (($messageType != 'messagesOnly' || $messageType != 'dlrsOnly') && $messageType == null) {
+            throw new \Exception('Please specify a valid message type');
+        }
+
+        $params = [
+            'accountname' => $this->client->getUsername(),
+            'password' => $this->client->getPassword(),
+            'maxmessagecount' => $maxMessageCount,
+        ];
+
+        if ($messageType == 'messagesOnly')
+            $params['messagesonly'] = 'true';
+
+        if ($messageType == 'dlrsOnly')
+            $params['dlrsonly'] = 'true';
+
+        $query = http_build_query($params);
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api2.toucantext.com/api/messages.json?' . urldecode($query));
+
+        return $response->getBody();
     }
 
     public function send()
